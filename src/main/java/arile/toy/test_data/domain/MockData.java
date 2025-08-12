2c5007980c957e0e2faf77272196f60bcf79874d
yesu0208 <dltmddnr0208@gmail.com>
2025-08-12 20:32:53 +0900

    #13 - 가짜 데이터 도메인 설계
    자동차 이름과 같은 고유명사처럼
    가짜 데이터를 사전식으로 저장해서 관리해야 하는 경우를
    도메인을 설계함.

---
+++ src/main/java/arile/toy/test_data/domain/MockData.java
@@ -0,0 +1,17 @@
+package arile.toy.test_data.domain;
+
+
+import arile.toy.test_data.domain.constant.MockDataType;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class MockData {
+
+    private MockDataType mockDataType;
+    private String mockDataValue;
+
+}
