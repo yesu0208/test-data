8a336b85ebcd663c223d25bf351a9480e15e8e7c
yesu0208 <dltmddnr0208@gmail.com>
2025-08-13 11:00:12 +0900

    #26 - 데이터베이스: DB를 표현할 entity 코드의 작성

--- src/main/java/arile/toy/test_data/domain/SchemaField.java
+++ src/main/java/arile/toy/test_data/domain/SchemaField.java
@@ -1,20 +1,82 @@
 package arile.toy.test_data.domain;
 
 import arile.toy.test_data.domain.constant.MockDataType;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-@Getter
-@Setter
-@ToString
-public class SchemaField {
+import java.util.Objects;
 
-    private String fieldName;
-    private MockDataType mockDataType;
-    private Integer fieldOrder;
-    private Integer blankPercent;
-    private String typeOptionJson;
-    private String forceValue;
-    
+
+/**
+ * 특정 {@link TableSchema}의 단위 필드 정보.
+ * 이 필드들이 모여서 테이블 스키마를 구성한다.
+ *
+ * @author Arile
+ */
+
+
+@Getter
+@ToString(callSuper = true)
+@Entity
+public class SchemaField extends AuditingFields{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
+    @ManyToOne(optional = false)
+    private TableSchema tableSchema;
+
+    @Setter private @Column(nullable = false) String fieldName;
+    @Setter private @Column(nullable = false) MockDataType mockDataType;
+    @Setter private @Column(nullable = false) Integer fieldOrder;
+    @Setter private @Column(nullable = false) Integer blankPercent;
+
+    @Setter private String typeOptionJson; // 이것은 없을 수도 있다. (Option)
+    @Setter private String forceValue; // 값을 랜던생성 안하고 수기로 직접 넣을 것인가? 없을 수도 있다.
+
+
+    protected SchemaField() {}
+
+    public SchemaField(String fieldName, MockDataType mockDataType,
+                       Integer fieldOrder, Integer blankPercent, String typeOptionJson, String forceValue){
+        this.fieldName = fieldName;
+        this.mockDataType = mockDataType;
+        this.fieldOrder = fieldOrder;
+        this.blankPercent = blankPercent;
+        this.typeOptionJson = typeOptionJson;
+        this.forceValue = forceValue;
+    }
+
+    public static SchemaField of(String fieldName, MockDataType mockDataType,
+                                 Integer fieldOrder, Integer blankPercent, String typeOptionJson, String forceValue) {
+        return new SchemaField(fieldName, mockDataType, fieldOrder, blankPercent, typeOptionJson, forceValue);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SchemaField that)) return false;
+
+        if (getId() == null) {
+            return Objects.equals(this.getFieldName(), that.getFieldName()) &&
+                    Objects.equals(this.getMockDataType(), that.getMockDataType()) &&
+                    Objects.equals(this.getFieldOrder(), that.getFieldOrder()) &&
+                    Objects.equals(this.getBlankPercent(), that.getBlankPercent()) &&
+                    Objects.equals(this.getTypeOptionJson(), that.getTypeOptionJson()) &&
+                    Objects.equals(this.getForceValue(), that.getForceValue());
+        }
+        return Objects.equals(this.getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        if (getId() == null){
+            return Objects.hash(getFieldName(), getMockDataType(), getFieldOrder(), getBlankPercent(), getTypeOptionJson(), getForceValue());
+        }
+        return Objects.hash(getId());
+    }
 }
--- src/main/java/arile/toy/test_data/domain/TableSchema.java
+++ src/main/java/arile/toy/test_data/domain/TableSchema.java
@@ -1,22 +1,104 @@
 package arile.toy.test_data.domain;
 
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 import java.time.LocalDateTime;
+import java.util.Collection;
+import java.util.LinkedHashSet;
+import java.util.Objects;
+import java.util.Set;
+
+/**
+ * 단위 테이블 스키마 정보.
+ * 식별자({@link #userId})로 특정할 수 있는 회원이 소유한다.
+ *
+ * @author Arile
+ */
 
 @Getter
-@Setter
-@ToString
-public class TableSchema {
+@ToString(callSuper = true)
+@Entity
+public class TableSchema extends AuditingFields{
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Setter
     private String schemaName;
+    @Setter
     private String userId;
+    @Setter
     private LocalDateTime exportedAt;
+
+    private LocalDateTime createdAt;
+    private String createdBy;
+    private LocalDateTime modifiedAt;
+    private String modifiedBy;
+
+    // final은 합당 : 여기에 새로운 LinkedHashSet을 넣는 것이 아닌 단순 add, delete만 하므로
+    @ToString.Exclude
+    @OneToMany(mappedBy = "tableSchema", cascade = CascadeType.ALL, orphanRemoval = true)
+    private final Set<SchemaField> schemaFields = new LinkedHashSet<>(); // 순서가 있는 set : 필드는 순서가 있다.
+
+
+    protected TableSchema() {
+    }
 
     public boolean isExported() {
         return exportedAt != null;
     }
 
+    public void makeExported() {
+        exportedAt = LocalDateTime.now();
+    }
+
+    public TableSchema(String schemaName, String userId) {
+        this.schemaName = schemaName;
+        this.userId = userId;
+        this.exportedAt = null;
+    }
+
+    public static TableSchema of(String schemaName, String userId) {
+        return new TableSchema(schemaName, userId);
+    }
+
+    public void addSchemaField(SchemaField schemaField){
+        schemaFields.add(schemaField);
+        schemaField.setTableSchema(this);
+    }
+
+    public void addSchemaFields(Collection<SchemaField> schemaFields){
+        schemaFields.forEach(this::addSchemaField);
+    }
+
+    public void clearSchemaFields() {
+        schemaFields.clear();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TableSchema that)) return false;
+
+        if (that.getId() == null) {
+            return Objects.equals(this.getSchemaName(), that.getSchemaName()) &&
+                    Objects.equals(this.getUserId(), that.getUserId()) &&
+                    Objects.equals(this.getExportedAt(), that.getExportedAt()) &&
+                    Objects.equals(this.getSchemaFields(), that.getSchemaFields());
+        }
+
+        return Objects.equals(this.getId(), that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        if (getId() == null) {
+            return Objects.hash(getSchemaName(), getUserId(), getExportedAt(), getSchemaFields());
+        }
+        return Objects.hash(getId());
+    }
 }
