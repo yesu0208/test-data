0f8947b6cabbbd23c725ba8b7447388b78b0cf54
yesu0208 <dltmddnr0208@gmail.com>
2025-08-14 13:39:16 +0900

    45 - 내 스키마 목록 dto에 수정일시 추가
    내 스키마 목록 화면에 수정일시 정보를 표시하기 위해
    데이터 스펙을 추가함.

--- src/main/java/arile/toy/test_data/dto/response/SimpleTableSchemaResponse.java
+++ src/main/java/arile/toy/test_data/dto/response/SimpleTableSchemaResponse.java
@@ -2,12 +2,15 @@ package arile.toy.test_data.dto.response;
 
 import arile.toy.test_data.dto.TableSchemaDto;
 
+import java.time.LocalDateTime;
+
 public record SimpleTableSchemaResponse(
         String schemaName,
-        String userId
+        String userId,
+        LocalDateTime modifiedAt
 ) {
 
     public static SimpleTableSchemaResponse fromDto(TableSchemaDto dto) {
-        return new SimpleTableSchemaResponse(dto.schemaName(), dto.userId());
+        return new SimpleTableSchemaResponse(dto.schemaName(), dto.userId(), dto.modifiedAt());
     }
 }
